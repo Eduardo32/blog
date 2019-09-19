5af87650bb28730b67ad3979311bbe11a674d742
Eduardo <p.eduardo.32@gmail.com>
2019-09-18 22:28:58 -0300

    Ajuste no model do app posts
    Ajuste para exclir a imagem da thumb caso o objeto seja atualizacom com uma nova imagem ou cosa o objeto seja excluido.

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,9 @@
+import os
 from django.db import models
 
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.dispatch import receiver
 
 
 class Post(models.Model):
@@ -50,3 +52,34 @@ class Post(models.Model):
         return "%s" % self.titulo
 
     objects = models.Manager()
+
+
+@receiver(models.signals.post_delete, sender=Post)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Remove o arquivo quando o objeto Post
+    correspondente for deletado
+    """
+    if instance.thumb:
+        if os.path.isfile(instance.thumb.path):
+            os.remove(instance.thumb.path)
+
+
+@receiver(models.signals.pre_save, sender=Post)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Remove o arquivo antigo quando o objeto Post
+    correspondente for atualizado com uma nova imagem
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Post.objects.get(pk=instance.pk).thumb
+    except Post.DoesNotExist:
+        return False
+
+    new_file = instance.thumb
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
